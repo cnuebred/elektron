ba4c3af3f2eabbb42901ca89d32b538c3a9d6167
cnuebred <jonatann01ew@gmail.com>
2020-08-07 00:52:52 +0200

    create main modules for set and load data

---
+++ src/command/commands/configuration/configuration.js
@@ -0,0 +1,25 @@
+const smartEmbed = require("../../../common/useful/smartEmbed")
+const globalVaribles = require("../../../common/object/globalVaribles")
+
+const options = globalVaribles.OPTIONS_CONFIGURATION
+
+const configuration = async (bot, msg, params) => {
+    if (!params[1])
+        return console.log('i can\'t find any params')
+    param = params[1].toLowerCase()
+
+    if (!options.includes(param))
+        return smartEmbed(bot, msg, {
+            description: `Dostępne opcje:\n>>> \`${options.join(', ')}\`\n`
+        })
+
+    const path = `set_${param}`
+    const method = require(`./options/${path}`)
+    if (method) {
+        return await method(bot, Object.assign(msg, { configurationOption: param }), params)
+    }
+}
+
+module.exports = {
+    method: configuration
+} 
